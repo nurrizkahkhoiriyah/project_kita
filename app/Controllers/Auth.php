dabd7deda863948f83fa0f2b50eeac5244610ad4
unknown <nrrizkahkh@gmail.com>
2024-07-01 21:30:05 +0700

    membuat halaman login_2

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -4,6 +4,11 @@ namespace App\Controllers;
 
 class Auth extends BaseController
 {
+    public function __construct()
+    {
+        helper('form');
+    }
+    
     public function index()
     {
         $data = [
@@ -11,5 +16,18 @@ class Auth extends BaseController
             'isi'   => 'v_login'
         ];
         return view('layout/v_wrapper', $data);
+    }
+
+    public function cek_login() 
+    {
+        $level = $this->request->getPost('level');
+
+        if($level == 1){
+            echo 'Admin';
+        }else if($level == 2){
+            echo 'Mahasiswa';
+        }elseif ($level == 3) {
+            echo 'Dosen';
+        }
     }
 }
